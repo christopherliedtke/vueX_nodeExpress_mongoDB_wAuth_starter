4285e3d53c254569365c501fd1bb38c95a80aad2
ggwoodsCL <ggwoods@gmx.de>
2020-06-18 09:51:28 +0200

    Add separate starter

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,18 +2,15 @@
     <header>
         <b-navbar toggleable="lg" type="dark">
             <b-navbar-brand to="/">
-                <img
-                    src="../assets/Logo_mfa-mal-anders-white.png"
-                    height="70"
-                    alt=""
-                />
+                <!-- <img src="../assets/logo.png" height="70" alt="" /> -->
+                <h2>BRAND</h2>
             </b-navbar-brand>
 
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="ml-auto">
-                    <b-nav-item to="/">Stellenanzeigen</b-nav-item>
+                    <b-nav-item to="/">Home</b-nav-item>
 
                     <b-nav-item-dropdown right>
                         <!-- Using 'button-content' slot -->
