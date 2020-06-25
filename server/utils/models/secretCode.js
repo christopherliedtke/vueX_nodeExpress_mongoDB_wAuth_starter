61ecbcd68142ce7b44f6f6d2b3d6040e66a3619b
ggwoodsCL <ggwoods@gmx.de>
2020-06-25 08:40:31 +0200

    Rename secretCode schema!

--- server/utils/models/secretCode.js
+++ server/utils/models/secretCode.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const pwResetSchema = new Schema({
+const secretCode = new Schema({
     email: {
         type: String,
         required: true,
@@ -17,4 +17,4 @@ const pwResetSchema = new Schema({
     },
 });
 
-module.exports.Code = mongoose.model("code", pwResetSchema);
+module.exports.Code = mongoose.model("code", secretCode);
